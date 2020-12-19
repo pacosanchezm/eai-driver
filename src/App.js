dba0bd6d0741e7b200cacc6e8c7526fe2f99e0d4
pacosanchezm <paco.sanchezm@gmail.com>
2020-12-19 05:57:58 -0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import App from "../eai-driver/appi";
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <React.StrictMode>
+    <App 
+     // id={3654898}
+      id={2437933}
+
+    />
+  </React.StrictMode>,
+  rootElement
+);
